04b83299893dc0c10c6d9474d57ed21fd8015faf
Rémi Vanicat <vanicat@debian.org>
2014-06-24 08:58:35 +0200

    theme

--- rc.lua
+++ rc.lua
@@ -185,7 +185,7 @@ else
 end
 
 -- Themes define colours, icons, font and wallpapers.
-beautiful.init("/usr/share/awesome/themes/default/theme.lua")
+beautiful.init("/home/moi/.config/awesome/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
 terminal = "x-terminal-emulator"
