9d588fb13f17576ef1f8ed7c9ef727711b515ee1
Rémi Vanicat <vanicat@debian.org>
2014-07-23 10:11:18 +0200

    Quit using my function.

--- rc.lua
+++ rc.lua
@@ -608,7 +608,9 @@ globalkeys = awful.util.table.join(
     -- Standard program
     awful.key({ modkey, "Control" }, "Return", function () awful.util.spawn(terminal) end),
     awful.key({ modkey, "Control" }, "r", awesome.restart),
-    awful.key({ modkey, "Shift"   }, "q", awesome.quit),
+    awful.key({ modkey, "Shift"   }, "q", function ()
+                                            awful.menu(quit_menu):show({ keygrabber = true })
+                                         end),
 
     awful.key({ modkey,           }, "l",     function () awful.tag.incmwfact( 0.05)    end),
     awful.key({ modkey,           }, "h",     function () awful.tag.incmwfact(-0.05)    end),
