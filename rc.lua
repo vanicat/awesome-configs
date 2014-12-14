b815608acb9480070827dfbb90806ee25235ee2d
Rémi Vanicat <vanicat@debian.org>
2014-12-14 10:26:19 +0100

    Removing some unused layout

--- rc.lua
+++ rc.lua
@@ -310,10 +310,10 @@ local layouts =
     -- awful.layout.suit.fair,
     awful.layout.suit.fair.horizontal,
     -- awful.layout.suit.spiral,
-    awful.layout.suit.spiral.dwindle,
+    -- awful.layout.suit.spiral.dwindle,
     awful.layout.suit.max,
     awful.layout.suit.max.fullscreen,
-    awful.layout.suit.magnifier,
+    -- awful.layout.suit.magnifier,
     awful.layout.suit.floating
 }
 -- }}}
